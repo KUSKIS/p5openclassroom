641ee30b3df44f1d5f6b14ac3c95c9375c96c2c3
KUSKIS <sabinemaillet@gmail.com>
2021-02-16 18:07:57 +0100

    home page
    + anime.js

--- views/template.php
+++ views/template.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.1/css/all.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.14.0/css/mdb.min.css" rel="stylesheet">
     <script src="https://cdn.tiny.cloud/1/uanhlhi99qxpp27vqve0o6abe0iulcyje5ybsdh0tbbihv82/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
@@ -14,12 +15,155 @@
 </head>
 
 <body>
+
+    <style>
+        html,
+        body,
+        header,
+        .view {
+            height: 100%;
+        }
+
+        @media (max-width: 740px) {
+
+            html,
+            body,
+            header,
+            .view {
+                height: 100vh;
+            }
+        }
+
+        .top-nav-collapse {
+            background-color: #333333 !important;
+        }
+
+        .navbar:not(.top-nav-collapse) {
+            background: transparent !important;
+        }
+
+        @media (max-width: 991px) {
+            .navbar:not(.top-nav-collapse) {
+                background: #333333 !important;
+            }
+        }
+
+        h1 {
+            letter-spacing: 20px;
+            font-size: 70px;
+        }
+
+        h5 {
+            letter-spacing: 3px;
+        }
+
+        .hr-light {
+            border-top: 3px solid #fff;
+            width: auto;
+
+        }
+    </style>
+
+    <!-- Main navigation -->
+    <header>
+        <!--Navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
+            <div class="container">
+                <a class="navbar-brand" href="#"><strong>EQUUS</strong></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-7" aria-controls="navbarSupportedContent-7" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent-7">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Profile</a>
+                        </li>
+                    </ul>
+                    <form class="form-inline">
+                        <div class="md-form my-0">
+                            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <!-- Navbar -->
+        <!-- Full Page Intro -->
+        <div class="view jarallax" data-jarallax='{"speed": 0.2}' style="background-image: url('public/pictures/saddle.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+            <!-- Mask & flexbox options-->
+            <div class="mask rgba-black-light d-flex justify-content-center align-items-center">
+                <!-- Content -->
+                <div class="container">
+                    <!--Grid row-->
+                    <div class="row">
+                        <!--Grid column-->
+                        <div class="col-md-12 pt-2 mb-4 white-text text-center">
+                            <h1 class="animation" class="h1-reponsive white-text text-uppercase font-weight-bold mb-0 pt-md-5 pt-5 wow fadeInDown" data-wow-delay="0.3s"><br><span class="word">PASSIONNEMENT CHEVAL</span><br><span class="word">PASSIONNEMENT E Q U U S</span></h1>
+                            <hr class="hr-light my-4 wow fadeInDown" data-wow-delay="0.4s">
+                            <h5 class="text-uppercase mb-4 white-text wow fadeInDown" data-wow-delay="0.4s"><strong>Partagez votre passion</strong></h5>
+                            <a class="btn btn-outline-white wow fadeInDown" data-wow-delay="0.4s">Inscription</a>
+                            <a class="btn btn-outline-white wow fadeInDown" data-wow-delay="0.4s">About me</a>
+                        </div>
+                        <!--Grid column-->
+                    </div>
+                    <!--Grid row-->
+                </div>
+                <!-- Content -->
+            </div>
+            <!-- Mask & flexbox options-->
+        </div>
+        <!-- Full Page Intro -->
+    </header>
+    <!-- Main navigation -->
+
+
     <?= $content ?>
 
+    <!-- Footer -->
+    <footer class="page-footer font-small darken-3 pt-4">
+
+        <!-- Footer Elements -->
+        <div class="container">
+
+            <!--Grid row-->
+            <div class="row d-flex justify-content-center">
+
+                <!--Grid column-->
+                <div class="col-md-6">
+
+                    
+                </div>
+                <!--Grid column-->
+
+            </div>
+            <!--Grid row-->
+
+        </div>
+        <!-- Footer Elements -->
+
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3">© 2018 Copyright:
+            <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+        </div>
+        <!-- Copyright -->
+
+    </footer>
+    <!-- Footer -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.14.0/js/mdb.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+
+    <script type="text/javascript" src="public/anime.js"></script>
 </body>
 
 </html>
--- views/viewHome.php
+++ views/viewHome.php
@@ -1,21 +1,255 @@
 <?php $this->title = "Equus Home Page"; ?>
 
-<nav class="navbar navbar bg-black">
-    <ul>
-        <li>
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#"><img src="public/pictures/logohorse.png" alt="logo" />EQUUS</a>
+
+
+
+
+
+
+
+
+<div class="posts">
+    <div class="container">
+    <!--Section: Content-->
+    <section class="text-center">
+
+        <!-- Section heading -->
+        <h3 class="font-weight-bold mb-5">Ils ont partagé !!!</h3>
+
+        <!-- Grid row -->
+        <div class="row">
+
+            <!-- Grid column -->
+            <div class="col-lg-4 col-md-12 mb-lg-0 mb-4">
+
+                <!-- Card -->
+                <div class="card hoverable">
+
+                    <!-- Card image -->
+                    <img class="card-img-top" src="public/pictures/horse.jpg" alt="Card image cap">
+
+                    <!-- Card content -->
+                    <div class="card-body">
+
+                        <!-- Title -->
+                        <p class="card-title text-muted text-uppercase font-small mt-1 mb-3">News</p>
+                        <!-- Text -->
+                        <p class="mb-2">We relocated our office to a new designed garage</p>
+
+                    </div>
+
+                </div>
+                <!-- Card -->
+
             </div>
-        </li>
-        <li>
-            <div class="ml-auto">
-                <form class="d-flex input-group w-auto">
-                    <input type="search" class="form-control" placeholder="Type query" aria-label="Search" />
-                    <button class="btn btn-outline-primary" type="button" data-mdb-ripple-color="dark">
-                        Search
-                    </button>
-                </form>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-lg-4 col-md-6 mb-md-0 mb-4">
+
+                <!-- Card -->
+                <div class="card hoverable">
+
+                    <!-- Card image -->
+                    <img class="card-img-top" src="public/pictures/horseshoe.jpg" alt="Card image cap">
+
+                    <!-- Card content -->
+                    <div class="card-body">
+
+                        <!-- Title -->
+                        <p class="card-title text-muted text-uppercase font-small mt-1 mb-3">Marketing</p>
+                        <!-- Text -->
+                        <p class="mb-2">Top five brilliant content marketing strategies</p>
+
+                    </div>
+
+                </div>
+                <!-- Card -->
+
             </div>
-        </li>
-    </ul>
-</nav>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-lg-4 col-md-6 mb-0">
+
+                <!-- Card -->
+                <div class="card hoverable">
+
+                    <!-- Card image -->
+                    <img class="card-img-top" src="public/pictures/horse.jpg" alt="Card image cap">
+
+                    <!-- Card content -->
+                    <div class="card-body">
+
+                        <!-- Title -->
+                        <p class="card-title text-muted text-uppercase font-small mt-1 mb-3">Design</p>
+                        <!-- Text -->
+                        <p class="mb-2">Best practices for minimalist design with example</p>
+
+                    </div>
+
+                </div>
+                <!-- Card -->
+
+            </div>
+            <!-- Grid column -->
+
+
+        </div>
+        <!-- Grid row -->
+
+    </section>
+    <!--Section: Content-->
+
+
+</div>
+</div>
+
+<hr>
+
+<div class="posts">
+    <div class="container">
+    <!--Section: Content-->
+    <section class="text-center">
+
+        <!-- Section heading -->
+        <h3 class="font-weight-bold mb-5">Ils ont partagé !!!</h3>
+
+        <!-- Grid row -->
+        <div class="row">
+
+            <!-- Grid column -->
+            <div class="col-lg-4 col-md-12 mb-lg-0 mb-4">
+
+                <!-- Card -->
+                <div class="card hoverable">
+
+                    <!-- Card image -->
+                    <img class="card-img-top" src="public/pictures/horse.jpg" alt="Card image cap">
+
+                    <!-- Card content -->
+                    <div class="card-body">
+
+                        <!-- Title -->
+                        <p class="card-title text-muted text-uppercase font-small mt-1 mb-3">News</p>
+                        <!-- Text -->
+                        <p class="mb-2">We relocated our office to a new designed garage</p>
+
+                    </div>
+
+                </div>
+                <!-- Card -->
+
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-lg-4 col-md-6 mb-md-0 mb-4">
+
+                <!-- Card -->
+                <div class="card hoverable">
+
+                    <!-- Card image -->
+                    <img class="card-img-top" src="public/pictures/horseshoe.jpg" alt="Card image cap">
+
+                    <!-- Card content -->
+                    <div class="card-body">
+
+                        <!-- Title -->
+                        <p class="card-title text-muted text-uppercase font-small mt-1 mb-3">Marketing</p>
+                        <!-- Text -->
+                        <p class="mb-2">Top five brilliant content marketing strategies</p>
+
+                    </div>
+
+                </div>
+                <!-- Card -->
+
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-lg-4 col-md-6 mb-0">
+
+                <!-- Card -->
+                <div class="card hoverable">
+
+                    <!-- Card image -->
+                    <img class="card-img-top" src="public/pictures/horse.jpg" alt="Card image cap">
+
+                    <!-- Card content -->
+                    <div class="card-body">
+
+                        <!-- Title -->
+                        <p class="card-title text-muted text-uppercase font-small mt-1 mb-3">Design</p>
+                        <!-- Text -->
+                        <p class="mb-2">Best practices for minimalist design with example</p>
+
+                    </div>
+
+                </div>
+                <!-- Card -->
+
+            </div>
+            <!-- Grid column -->
+
+
+        </div>
+        <!-- Grid row -->
+
+    </section>
+    <!--Section: Content-->
+
+
+</div>
+</div>
+
+<div class="article">
+  <div class="container sm my-5 py-5 z-depth-1">
+
+
+    <!--Section: Content-->
+    <section class="px-md-5 mx-md-5 dark-grey-text text-center text-lg-left">
+
+      <!--Grid row-->
+      <div class="row">
+
+        <!--Grid column-->
+        <div class="col-lg-6 mb-4 mb-lg-0 d-flex align-items-center justify-content-center">
+
+          <!-- Video -->
+                    <div class="embed-responsive embed-responsive-16by9 mb-4">
+                        <iframe class="embed-responsive-item" src="https://youtu.be/UpCHufqos9I" allowfullscreen></iframe>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/UpCHufqos9I" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+
+
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-6 mb-4 mb-lg-0">
+
+          <h3 class="font-weight-bold">Partagez vos moments</h3>
+
+          <p class="font-weight-bold">That's a very nice subheading</p>
+
+          <p class="text-muted">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id quam sapiente
+            molestiae
+            numquam quas, voluptates omnis nulla ea odio quia similique corrupti magnam, doloremque laborum.</p>
+
+          <a class="font-weight-bold" href="#" >Learn more<i class="fas fa-angle-right ml-2"></i></a>
+
+        </div>
+        <!--Grid column-->
+
+      </div>
+      <!--Grid row-->
+
+
+    </section>
+    <!--Section: Content-->
+
+
+  </div>
+</div>
